641a2be642438bc6f8c5e64fdfe764369750bdcc
Livrädo Sandoval <104039397+livrasand@users.noreply.github.com>
2024-05-24 14:40:53 -0600

    Actualización a la v1.2.1

--- create_zip_package.py
+++ create_zip_package.py
@@ -11,11 +11,27 @@ class CreateZipPackageCommand(sublime_plugin.WindowCommand):
         zip_filename = os.path.join(current_folder, folder_name + ".jwpub")
 
         try:
-            with zipfile.ZipFile(zip_filename, "w") as zipf:
-                for root, _, files in os.walk(current_folder):
+            # Comprimir "contents~" en "contents.zip"
+            contents_folder = os.path.join(current_folder, "contents~")
+            contents_zip_filename = os.path.join(current_folder, "contents.zip")
+            with zipfile.ZipFile(contents_zip_filename, "w") as contents_zipf:
+                for root, dirs, files in os.walk(contents_folder):
                     for file in files:
                         file_path = os.path.join(root, file)
-                        zipf.write(file_path, os.path.relpath(file_path, current_folder))
+                        contents_zipf.write(file_path, os.path.relpath(file_path, contents_folder))
+                    for dir in dirs:
+                        dir_path = os.path.join(root, dir)
+                        contents_zipf.write(dir_path, os.path.relpath(dir_path, contents_folder))
+
+            # Renombrar "contents.zip" a "contents"
+            os.rename(contents_zip_filename, os.path.join(current_folder, "contents"))
+
+            # Comprimir "contents" y "manifest.json" en ".jwpub"
+            with zipfile.ZipFile(zip_filename, "w") as zipf:
+                for item in ["contents", "manifest.json"]:
+                    item_path = os.path.join(current_folder, item)
+                    zipf.write(item_path, os.path.relpath(item_path, current_folder))
+
         except Exception as e:
             sublime.error_message("Error creating JWPUB package: {}".format(e))
             return
